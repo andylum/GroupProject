03c408a1ee9c21658fa067b6acd808f93f846e59
steagray <steagray@ut.utm.edu>
2021-11-06 04:28:29 -0500

    Expanded on gameWindow

--- GroupProject/MainWindow.xaml.cs
+++ GroupProject/MainWindow.xaml.cs
@@ -27,23 +27,52 @@ namespace GroupProject
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            //This pops up a window.
+            //NOTE: A lot of this is just demo code. Including this function.
+
+            //Creates the basic window object
             Window gameWindow = new Window();
             gameWindow.Height = 400;
             gameWindow.Width = 800;
 
-            //create Grid to place stuff on
+            //create Grid to add information onto
             Grid mainGrid = new Grid();
+            mainGrid.RenderSize = new Size(gameWindow.Width, gameWindow.Height);
 
             //TODO: Fill gameWindow with stuff needed in the game window, as indicated by the drawing. 
             //Info Site: https://www.c-sharpcorner.com/UploadFile/mahesh/using-xaml-image-in-wpf/
-            Image dynamicImage = new Image();
-            dynamicImage.Width = 100;
-            dynamicImage.Height = 100;
-            BitmapImage newBtmpImg = new BitmapImage(new Uri("https://fontmeme.com/images/undertale-font.jpg"));
-            dynamicImage.Source = newBtmpImg;
+            //Logo
+            Image gameLogo = new Image();
+            gameLogo.Width = 230;
+            gameLogo.Height = 107;
+            BitmapImage newBtmpImg = new BitmapImage(new Uri("https://cdn.cloudflare.steamstatic.com/steam/apps/391540/header.jpg"));
+            gameLogo.Source = newBtmpImg;
+            gameLogo.HorizontalAlignment = HorizontalAlignment.Left;
+            gameLogo.VerticalAlignment = VerticalAlignment.Top;
+            gameLogo.Margin = new Thickness(0, 0, 0, 0);
 
-            mainGrid.Children.Add(dynamicImage);
+            //Game Name
+            TextBlock gameName = new TextBlock();
+            gameName.Text = "Name: Undertale";
+            gameName.HorizontalAlignment = HorizontalAlignment.Left;
+            gameName.VerticalAlignment = VerticalAlignment.Top;
+            gameName.Margin = new Thickness(0, gameLogo.Height, 0, 0);
+            gameName.FontSize = 16;
+            gameName.FontFamily = new FontFamily("Comic Sans MS");
+
+            //Game Descritpion
+            TextBlock gameDesc = new TextBlock();
+            gameDesc.Text = "This is the description for popular Independent Video Game Undertale.";
+            gameDesc.HorizontalAlignment = HorizontalAlignment.Left;
+            gameDesc.VerticalAlignment = VerticalAlignment.Top;
+            gameDesc.Margin = new Thickness(0, gameLogo.Height + 20, 0, 0);
+            gameDesc.FontSize = 16;
+            gameDesc.FontFamily = new FontFamily("Comic Sans MS");
+
+            //Add things needed in the gameWindow to the Grid
+            mainGrid.Children.Add(gameLogo);
+            mainGrid.Children.Add(gameName);
+            mainGrid.Children.Add(gameDesc);
+
             gameWindow.Content = mainGrid;
             
             gameWindow.Show();
